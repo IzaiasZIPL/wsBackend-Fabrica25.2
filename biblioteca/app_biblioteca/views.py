40f02417780c5ce902c3f0c85807a48b90669dbd
IzaiasZIPL <Izaiasjrterto@gmail.com>
2025-09-13 17:34:28 -0300

    ✨feat(views) adiciona a função de cadastro de estudantes na views, mudan template HTML

--- biblioteca/app_biblioteca/views.py
+++ biblioteca/app_biblioteca/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import Livro#, Estudante
+from .models import Livro, Estudante
 #from .forms import PessoaForm
 
 def home(request):
     return render(request, 'home.html')
-
+#viws de livros
 def cadastrar_livro(request):
   if request.method == 'GET':
     return render(request, 'app_biblioteca/livros/cadastrar_livros.html')
@@ -56,3 +56,25 @@ def atualizar_livro(request, id):
 def listar_livros(request):
     livros = Livro.objects.all()
     return render(request, 'app_biblioteca/livros/listar_livros.html', {'livros': livros})
+
+#views de estudantes
+
+def cadastrar_estudante(request):
+  if request.method == 'GET':
+    return render(request, 'app_biblioteca/estudantes/cadastrar_estudantes.html')
+  elif request.method == 'POST':
+     nome = request.POST.get('nome')
+     email = request.POST.get('email')
+     dataDeNascimento = request.POST.get('dataDeNascimento')
+     telefone = request.POST.get('telefone')
+
+     estudante = Estudante(
+        nome=nome,
+        email=email,
+        dataDeNascimento=dataDeNascimento,
+        telefone=telefone
+    )
+
+     estudante.save()
+     return redirect('cadastrar_estudante')
+
